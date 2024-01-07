e7f7bb81779ad6d6571d74aff8a712d0723c6c22
moldypeaches <adelejulietmartin@gmail.com>
2024-01-07 17:31:22 +0100

    ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,38 @@
 /*   By: ademarti <adelemartin@student.42.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 16:57:39 by ademarti          #+#    #+#             */
-/*   Updated: 2024/01/07 16:58:09 by ademarti         ###   ########.fr       */
+/*   Updated: 2024/01/07 17:31:03 by ademarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
+#include <unistd.h>
 
-int open (const char* path, int flags [, int mode ]);
-
-char *get_next_line(int fd)
+/*
+int open(const char* path, int flags)
 {
+	int fd;
+	fd =
 
-open();
-read();
+	if (fd > 0)
+        write(fd, &c, 1);
+    else
+        write(2, "test.txt not found.\n", 20);
+		return (-1);
 
+	return (1);
+}
+
+void *get_next_line(int fd, char c)
+{
+	fd = open("/Users/adelemartin/fd_getnextline.rtf", O_RDONLY);
+
+
+}
+*/
+
+int main()
+{
+ int fd;
+fd = open("text.txt", O_RDONLY);
 }
