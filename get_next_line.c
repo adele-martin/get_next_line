9f3a28c44d3ba0f6306e154d3e38beb1b4398f5d
Adele Martin <ademarti@c1b2c3.42berlin.de>
2024-01-09 14:16:21 +0100

    ok

--- get_next_line.c
+++ get_next_line.c
@@ -3,30 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ademarti <adelemartin@student.42.fr>       +#+  +:+       +#+        */
+/*   By: ademarti <ademarti@student.42berlin.de     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 16:57:39 by ademarti          #+#    #+#             */
-/*   Updated: 2024/01/07 17:49:19 by ademarti         ###   ########.fr       */
+/*   Updated: 2024/01/09 14:16:05 by ademarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <unistd.h>
+#include "get_next_line.h"
+#include "stdio.h" //to delete later
 
-int *get_next_line(int fd)
+char *put_in_stash(int fd)
 {
-	if (fd > 0)
-		write(fd, &c, 1);
-	 else
-		write(2, "test.txt not found.\n", 20);
-		return (-1);
+	static char *stash;
+	char *buffer;
+	buffer = (char*)malloc(BUFFER_SIZE);
 
-	return (0);
+	ssize_t bytesRead;
+    while ((bytesRead = read(fd, buffer, BUFFER_SIZE)) > 0) {
+         write(1, buffer, bytesRead);
+    }
+	//printf("%s", buffer);
+	return (buffer);
+}
+
+char *get_next_line(int fd)
+{
+	//make_line();
+	//update_stash();
+	return (put_in_stash(fd));
 }
 
 int main()
 {
 	int fd;
-	fd = open("/Users/adelemartin/fd_getnextline.rtf", O_RDONLY);
+	fd = open("text.txt", O_RDONLY);
+	get_next_line(fd);
+	close(fd);
+// FT_STRCHR
+// FT_STRDUP
+// FT_STRLEN
+// FT_SUBSTR
+// FT_STRJOIN
 
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademarti <ademarti@student.42berlin.de     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/08 14:53:38 by ademarti          #+#    #+#             */
+/*   Updated: 2024/01/09 14:01:02 by ademarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
 
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 5
+# endif
 
 #endif
-
-
-
-ssize_t read(int fildes, void *buf, size_t nbyte)
-{
-	buf =
-}
